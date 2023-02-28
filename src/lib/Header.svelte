669c2ed3b772454243edb8827c348d9f9c7790de
Gabriel Neutzling <gabrielneutzling@gmail.com>
2023-02-28 19:26:44 -0300

    initial code

---
+++ src/lib/Header.svelte
@@ -0,0 +1,17 @@
+<script>
+  export let username;
+</script>
+
+<herader>
+  <h2>{username}</h2>
+</herader>
+
+<style>
+  header {
+    padding: 2rem;
+  }
+
+  h2 {
+    color: red;
+  }
+</style>
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,6 @@
+<script>
+  let pageName = "ABOUT";
+</script>
+
+<h1>{pageName}!</h1>
+<a href="/">home</a>
