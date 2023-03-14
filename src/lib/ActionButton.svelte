5c261570ec4b6323a67ab25f6af0a76d61d84823
Gabriel Neutzling <gabrielneutzling@gmail.com>
2023-03-13 22:54:08 -0300

    add action button component to be displayed in the entry page

---
+++ src/lib/ActionButton.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  export let href: string;
+</script>
+
+<a
+  {href}
+  class="flex justify-center items-center w-full p-10 bg-orange-300 rounded-md"
+>
+  <span class="text-center block uppercase font-bold text-lg">
+    <slot />
+  </span>
+</a>
