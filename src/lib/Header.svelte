b3584824c33acfaeff456fc41352b11eee93a8e7
Gabriel Neutzling <gabrielneutzling@gmail.com>
2023-03-13 22:52:30 -0300

    update header component to show app name

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,17 +1,3 @@
-<script>
-  export let username;
-</script>
-
-<herader>
-  <h2>{username}</h2>
-</herader>
-
-<style>
-  header {
-    padding: 2rem;
-  }
-
-  h2 {
-    color: red;
-  }
-</style>
+<header class="w-full py-4">
+  <h1 class="text-center">Forever Moments</h1>
+</header>
