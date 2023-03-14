7deac8c473646bd4c498e6cc0bcedb270f308316
Gabriel Neutzling <gabrielneutzling@gmail.com>
2023-03-13 21:01:34 -0300

    add tailwind to the project

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script>
+  import "../app.css";
+</script>
+
+<div style="background: yellow">
+  <slot />
+</div>
+
+<style lang="postcss">
+  :global(html) {
+    background-color: theme(colors.gray.100);
+  }
+</style>
